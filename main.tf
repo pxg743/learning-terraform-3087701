2a5a446112be7d74bfd6473b02b5f2dcd8ec2d40
pxg743 <chris.hakimi@outlook.com>
2024-02-10 18:46:22 -0800

    refactor number three

--- main.tf
+++ main.tf
@@ -62,5 +62,38 @@ module "blog_sg" {
 
 }
 
+module "nlb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name               = "my-nlb"
+  load_balancer_type = "network"
+  vpc_id             = "vpc-abcde012"
+  subnets            = ["subnet-abcde012", "subnet-bcde012a"]
+
+  # Security Group
+  enforce_security_group_inbound_rules_on_private_link_traffic = "on"
+
+  security_group_ingress_rules = module.blog_sg.ingress_rules ["http-80-tcp"]
+  security_group_egress_rules  = module.blog_vpc.egress_rules ["all-all"]
+
+  access_logs = {
+    bucket = "blog-logs"
+  }
+
+
+  target_groups = {
+    ex-target = {
+      name_prefix = "blog-"
+      protocol    = "TCP"
+      port        = 80
+      target_type = "ip"
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
 
 
