05fec5021fbe1f017434fa5b05381d5f64818ad7
pxg743 <chris.hakimi@outlook.com>
2024-02-10 17:18:20 -0800

    clear display name

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  name    = "bloc_new_sg"
+  name    = "blog"
   vpc_id  = data.aws_vpc.default.id
 
   ingress_rules = [ "http-80-tcp", "https-443-tcp"]
